851dcf91def9a6ff35008d9c916851d5672f5e70
Tyler Irwin <tyler.s.irwin@gmail.com>
2014-03-29 03:55:02 -0500

    Update and rename slim.mk to ose.mk
    ose ready hercules

--- ose.mk
+++ ose.mk
@@ -6,19 +6,16 @@ TARGET_SCREEN_WIDTH := 480
 $(call inherit-product, device/samsung/hercules/full_hercules.mk)
 
 # Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
-
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/gsm.mk)
+$(call inherit-product, vendor/ose/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/ose/config/nfc_enhanced.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := hercules
 
 # Device identifier
-PRODUCT_NAME := slim_hercules
+PRODUCT_NAME := ose_hercules
 PRODUCT_DEVICE := hercules
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 TARGET_DEVICE=SGH-T989 BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.3/JZO54K/T989UVMC6:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.3 IMM76D UVMC6 release-keys"
